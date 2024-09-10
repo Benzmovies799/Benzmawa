d1ccae84a9fa2f2bfe12e158720c721008eb54eb
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2024-09-10 21:34:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6589241609:AAEloFnJBjm2KxOByB--NSuMKI4U5w7
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', ' https://envs.sh/P0O.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', ' https://graph.org/file/2d20cb938f67aa3f94876.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
